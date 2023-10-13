9349ad0715a93daa92a745dcdf729ec057541c2b
Shawn Teh <steh@u91z03s02.42kl.edu.my>
2023-10-13 21:49:17 +0800

    broiler plate for bridge

---
+++ TokenEthTestnet.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.0;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TokenEthTestnet is ERC20 {
+    constructor(uint256 _initialSupply) ERC20("TokenEthTestnet", "TET") {
+        _mint(msg.sender, _initialSupply * (10 ** uint256(10)));
+        console.log("Tokens are successfully minted %s", _initialSupply);
+        console.log("Contract deployed! Tokens sent to %s", msg.sender);
+    }
+}
