ee7ab6700e1fa35aaea840a969279f734c0a4fb2
Shawn Teh <steh@u91z03s02.42kl.edu.my>
2023-10-14 15:20:46 +0800

    add hardhat/console.sol

--- solidity/contracts/TokenEthTestnet.sol
+++ solidity/contracts/TokenEthTestnet.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.8.0;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "hardhat/console.sol";
 
 contract TokenEthTestnet is ERC20 {
     constructor(uint256 _initialSupply) ERC20("TokenEthTestnet", "TET") {
--- solidity/contracts/TokenMantleTestnet.sol
+++ solidity/contracts/TokenMantleTestnet.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "hardhat/console.sol";
 
 contract TokenMantleTestnet is ERC20, ERC20Burnable {
     address bridge;
