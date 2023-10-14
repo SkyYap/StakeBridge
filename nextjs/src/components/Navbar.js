e608652acce334984f815dda4f04774192add757
1HoWK <howaikit01@gmail.com>
2023-10-14 18:08:58 +0800

    Fixed connect button

--- nextjs/src/components/Navbar.js
+++ nextjs/src/components/Navbar.js
@@ -1,16 +1,17 @@
-import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-
-import { ethers } from "ethers";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import ButtonBase from '@mui/material/ButtonBase';
+import WalletIcon from '@mui/icons-material/Wallet';
+import { ethers } from 'ethers';
 
 export default function ButtonAppBar() {
   const [walletConnection, setWalletConnection] = React.useState();
   const connectToWallet = async () => {
-    console.log("Connecting to wallet");
+    console.log('Connecting to wallet');
     console.log(walletConnection);
 
     if (window.ethereum) {
@@ -25,27 +26,35 @@ export default function ButtonAppBar() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="fixed" sx={{ backgroundColor: "#FFFFFF" }}>
+      <AppBar position='fixed' sx={{ backgroundColor: '#FFFFFF' }}>
         <Toolbar>
           <Typography
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1, color: "#2563eB" }}
+            variant='h6'
+            component='div'
+            sx={{ flexGrow: 1, color: '#2563eB' }}
           >
             Web3Xplorer-CrossChain
           </Typography>
           {walletConnection ? (
-            <Typography variant="body1" sx={{ color: "#2563eB" }}>
+            <Typography
+              variant='caption'
+              sx={{ color: '#2563eB', border: '1px solid', p: 1 }}
+            >
               Your Contract Address: {walletConnection}
             </Typography>
           ) : (
-            <Button
-              color="inherit"
+            <ButtonBase
               onClick={() => connectToWallet()}
-              sx={{ color: "#ffffff", backgroundColor: "#2563eB", borderRadius: "1.5rem" }}
+              sx={{
+                color: '#ffffff',
+                backgroundColor: '#2563eB',
+                borderRadius: '1.5rem',
+                fontSize: '0.7rem',
+                p: 0.6,
+              }}
             >
-              Connect Wallet
-            </Button>
+              <WalletIcon fontSize='large' sx={{ mr: 0.5 }} /> Connect Wallet
+            </ButtonBase>
           )}
         </Toolbar>
       </AppBar>
