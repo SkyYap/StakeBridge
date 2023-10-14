66c7fec49a8d8b26b9f5ba56e88c21cdfa292be4
shawnteh1711 <fongsiongshawn@gmail.com>
2023-10-14 15:31:42 +0800

    update

--- solidity/contracts/TokenEthTestnet.sol
+++ solidity/contracts/TokenEthTestnet.sol
@@ -7,6 +7,6 @@ contract TokenEthTestnet is ERC20 {
     constructor(uint256 _initialSupply) ERC20("TokenEthTestnet", "TET") {
         _mint(msg.sender, _initialSupply * (10 ** uint256(10)));
         console.log("Tokens are successfully minted %s", _initialSupply);
-        console.log("Contract deployed! Tokens sent to %s", msg.sender);
+        console.log("Contract deployed! Tokens sent to the %s", msg.sender);
     }
 }
